68263d5d0de79511a94e4db464d6b9bbade24e54
Jay Cribas <feverjay@gmail.com>
2017-09-29 15:50:45 -0700

    Refactors login POST to abstract compare hash utility

--- src/server/utils.js
+++ src/server/utils.js
@@ -4,8 +4,12 @@ const hashPassword = function(plainTextPassword){
   return bcrypt.hash(plainTextPassword, 10)
 }
 
+const compareHash = function(plainTextPassword, hash){
+  return bcrypt.compare(plainTextPassword, hash)
+}
+
 const renderError = function(error, response, response){
   response.send(`ERROR: ${error.message}\n\n${error.stack}`)
 }
 
-module.exports = {hashPassword,renderError}
+module.exports = {hashPassword,renderError,compareHash}
