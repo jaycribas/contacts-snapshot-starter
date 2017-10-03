feb8fe937f8580e6b2f9fa9a4b3ec660aeab462f
Jay Cribas <feverjay@gmail.com>
2017-10-03 15:04:53 -0700

    Moves middlewares from server into routes, adds page titles

--- src/server/routes/users.js
+++ src/server/routes/users.js
@@ -1,36 +1,36 @@
-const db = require('../../models/users')
+const users = require('../../models/users')
 const router = require('express').Router()
 const {hashPassword,renderError,compareHash} = require('../utils')
 
 router.route('/login')
   .get((request, response,) => {
-    response.render('users/login', {warning: ''})
+    response.render('users/login', {title: 'Log In'})
   })
   .post((request, response, next) => {
     const user = {
       username: request.body.username,
       password: request.body.password
     }
-    db.find(user.username)
+    users.find(user.username)
     .then(response => compareHash(user.password, response[0].password))
     .then(match => {
       if(match){
         request.session.user = user
         response.redirect('/')
       } else {
-        response.render('users/login', {warning: 'Invalid username or password'})
+        response.status(401).render('users/login', {title: 'Log In', warning: 'Invalid username or password'})
       }
     }).catch(error => next(error))
   })
 
 router.route('/signup')
   .get((request, response) => {
-    response.render('users/signup')
+    response.render('users/signup', {title: 'Sign Up'})
   })
   .post((request, response, next) => {
     const { username, password } = request.body
     hashPassword(password)
-    .then(hash => db.create({username: username, password: hash}))
+    .then(hash => users.create({username: username, password: hash}))
     .then(user => {
       request.session.user = user
       response.redirect('/')
